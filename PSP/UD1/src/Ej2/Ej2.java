78f8c1d5544a34dfef7cf38ef18a8014160791f6
elnavarro2007 <diegonavarrofernandez07@gmail.com>
2026-09-24 21:03:10 +0200

    si las sirenas fueran lesbianas como harian tijeras

--- PSP/UD1/src/Ej2/Ej2.java
+++ PSP/UD1/src/Ej2/Ej2.java
@@ -3,8 +3,13 @@ package Ej2;
 import java.io.IOException;
 
 public class Ej2 {
-    static void main(String[] args) throws IOException {
-        ProcessBuilder p = new ProcessBuilder("");
-        Process process = p.start();
+    static void main(String[] args)  {
+        try {
+            ProcessBuilder p = new ProcessBuilder("cmd.exe");
+            Process process = p.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
